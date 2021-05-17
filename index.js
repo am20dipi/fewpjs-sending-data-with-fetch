786d10d3ab3768bdf010a16505b8d1cba204f423
Adriana <am20dipi@alum.siena.edu>
2021-05-17 15:32:17 -0400

    commit tree f714bf8d4500ace0cc124352208d280e786704e6

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Add your code here
+/* let formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle"
+}; */
+
+
+/* let configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+}; */
+
+/* fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        console.log(object);
+    });
+ */
+
+/* let userData = {
+    userName: "Bob",
+    userEmail: "bob@gmail.com"
+} */
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name, 
+            email
+        })
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    })
+
+}
+
